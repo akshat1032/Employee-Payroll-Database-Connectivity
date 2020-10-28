d861053ac5cc1b1d1cd8c1433d092798720c3599
akshat1032 <akshat.30oct@gmail.com>
2020-10-28 17:17:19 +0530

    Added : Welcome message added

---
+++ src/main/java/com/capgemini/payrollservicedbconnectivity/PayrollServiceDBConnectivity.java
@@ -0,0 +1,13 @@
+package com.capgemini.payrollservicedbconnectivity;
+
+import java.util.logging.*;
+
+public class PayrollServiceDBConnectivity {
+
+	public static void main(String[] args) {
+
+		Logger log = Logger.getLogger(PayrollServiceDBConnectivity.class.getName());
+		// Welcome message
+		log.info("Welcome to Payroll Service Connectivity");
+	}
+}
