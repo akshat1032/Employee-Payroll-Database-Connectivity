f69b352e9e316c8ccf6f6153d95e4ff7e113da15
akshat1032 <akshat.30oct@gmail.com>
2020-10-28 18:51:02 +0530

    Added : Retrieve employee data from database added

--- src/main/java/com/capgemini/payrollservicedbconnectivity/PayrollServiceDBConnectivity.java
+++ src/main/java/com/capgemini/payrollservicedbconnectivity/PayrollServiceDBConnectivity.java
@@ -2,6 +2,9 @@ package com.capgemini.payrollservicedbconnectivity;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.sql.Driver;
 import java.util.Enumeration;
 import java.util.logging.*;
@@ -9,14 +12,14 @@ import java.util.logging.*;
 public class PayrollServiceDBConnectivity {
 
 	private static Logger log = Logger.getLogger(PayrollServiceDBConnectivity.class.getName());
-	
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws DatabaseException {
 
 		
 		String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
 		String userName = "root";
 		String password = "123qwe";
-		Connection con;
+		Connection con = null;
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			log.info("Driver loaded");
@@ -33,9 +36,25 @@ public class PayrollServiceDBConnectivity {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		Statement stmt = null;
+		ResultSet employeePayroll = null;
+		try {
+			stmt = con.createStatement();
+			employeePayroll = stmt.executeQuery("select * from employee_payroll");
+			while (employeePayroll.next()) {
+				log.info(employeePayroll.getInt(1)+" "+employeePayroll.getString(2)+" "
+						+employeePayroll.getString(3)+" "+employeePayroll.getString(4)+" "
+						+employeePayroll.getString(5)+" "+employeePayroll.getString(6)+" "
+						+employeePayroll.getDouble(7)+" "+employeePayroll.getDouble(8)+" "
+						+employeePayroll.getDouble(9)+" "+employeePayroll.getDouble(10)+" "
+						+employeePayroll.getDouble(11)+" "+employeePayroll.getDate(12));
+			}
+			}catch(SQLException e) {
+				throw new DatabaseException("Error in retrieving data from database");
+			}
 	}
-	
-	// Printing the available classes	
+
+	// Printing the available classes
 	private static void listOfDrivers() {
 		Enumeration<Driver> driverList = DriverManager.getDrivers();
 		while (driverList.hasMoreElements()) {
